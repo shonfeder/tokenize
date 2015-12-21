2c0e56bd408ba9ccdc18ef88af19024e148722c8
Shon Feder <aBathologist@gmail.com>
2015-12-21 02:18:17 -0500

    Began tracking pack.pl.

---
+++ pack.pl
@@ -0,0 +1,7 @@
+name(tokenize).
+title('A nascent tokenization library').
+
+version('0.1.0').
+author('Shon Feder', 'shon.feder@gmail.com').
+packager('Shon Feder', 'shon.feder@gmail.com').
+maintainer('Shon Feder', 'shon.feder@gmail.com').
