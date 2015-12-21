6b415163f38dc4138aed08868989bbc43d979e31
Shon Feder <aBathologist@gmail.com>
2015-12-21 17:29:22 -0500

    Updated pack.pl with website info.

--- pack.pl
+++ pack.pl
@@ -2,6 +2,9 @@ name(tokenize).
 title('A nascent tokenization library').
 
 version('0.1.0').
+download('https://github.com/aBathologist/tokenize/archive/master.zip').
+
 author('Shon Feder', 'shon.feder@gmail.com').
 packager('Shon Feder', 'shon.feder@gmail.com').
 maintainer('Shon Feder', 'shon.feder@gmail.com').
+home('https://github.com/aBathologist/tokenize').
