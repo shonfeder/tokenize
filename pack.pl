53eae78733c2d13ee2854c2d08e6d389971b044e
Shon Feder <aBathologist@gmail.com>
2015-12-21 21:31:52 -0500

    added note to pack.pl

--- pack.pl
+++ pack.pl
@@ -2,6 +2,8 @@ name(tokenize).
 title('A nascent tokenization library').
 
 version('0.1.1').
+%% Remember to change this to enable automatic update checking next time!
+%% Cf. http://eu.swi-prolog.org/howto/Pack.html
 download('https://github.com/aBathologist/tokenize/archive/v0.1.1.zip').
 
 author('Shon Feder', 'shon.feder@gmail.com').
