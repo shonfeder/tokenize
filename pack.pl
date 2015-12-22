f3903626da850cbe1c13822edd40f7dfec988f83
Shon Feder <aBathologist@gmail.com>
2015-12-21 23:25:23 -0500

    Added download url to enable auto update checking.

--- pack.pl
+++ pack.pl
@@ -1,10 +1,10 @@
 name(tokenize).
 title('A nascent tokenization library').
 
-version('0.1.1').
+version('0.1.2').
 %% Remember to change this to enable automatic update checking next time!
 %% Cf. http://eu.swi-prolog.org/howto/Pack.html
-download('https://github.com/aBathologist/tokenize/archive/v0.1.1.zip').
+download('https://github.com/aBathologist/tokenize/release/*.zip').
 
 author('Shon Feder', 'shon.feder@gmail.com').
 packager('Shon Feder', 'shon.feder@gmail.com').
