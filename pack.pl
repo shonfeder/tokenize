19a4b44e941c0376e6f953cb1bed690c61ed6fd0
Shon Feder <aBathologist@gmail.com>
2015-12-21 20:48:49 -0500

    Corrected download url and version info in pack.pl

--- pack.pl
+++ pack.pl
@@ -1,8 +1,8 @@
 name(tokenize).
 title('A nascent tokenization library').
 
-version('0.1.0').
-download('https://github.com/aBathologist/tokenize/archive/v0.1.0.zip').
+version('0.1.1').
+download('https://github.com/aBathologist/tokenize/archive/v0.1.1.zip').
 
 author('Shon Feder', 'shon.feder@gmail.com').
 packager('Shon Feder', 'shon.feder@gmail.com').
